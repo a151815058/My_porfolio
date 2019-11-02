e1c31afb6a21c87da9b36506a4be57a06240a4e0
姚佩彤 <a151815058@gmail.com>
2019-11-02 18:58:36 +0800

    Implemented images deault for portfolio items

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -6,4 +6,12 @@ class Portfolio < ApplicationRecord
 	end
 
 	scope :ruby_on_rails_portfolio_items, -> {where(subtitle: 'Ruby on rails')}
+
+	after_initialize :set_defaults
+
+	#在create之後，系統會自動產生main與thumb的預設
+	def set_defaults
+		self.main_image ||= "https://via.placeholder.com/600X400.png?"
+		self.thumb_image ||= "https://via.placeholder.com/350X200.png?"
+	end
 end
