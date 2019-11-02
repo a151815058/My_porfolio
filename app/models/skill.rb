0d2a51bcef1f7abcc0c1491de714ade2768d84d2
姚佩彤 <a151815058@gmail.com>
2019-11-02 19:28:02 +0800

    Implemented concern for managing image data

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,3 +1,12 @@
 class Skill < ApplicationRecord
+	include Placeholder
 	validates_presence_of  :title, :percent_utilized
+
+		after_initialize :set_defaults
+
+	#在create之後，系統會自動產生main與thumb的預設
+	def set_defaults
+		self.badge ||= Placeholder.image_generator(height: '250 ',width: '250')
+	end
 end
+ 
--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,4 @@
-  # This file is auto-generated from the current state of the database. Instead
+# This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191102080110) do
+ActiveRecord::Schema.define(version: 20191102110549) do
 
   create_table "blogs", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "title"
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 20191102080110) do
     t.integer "percent_utilized"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "badge"
   end
 
   create_table "topics", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
