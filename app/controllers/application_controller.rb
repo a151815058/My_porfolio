7a565d31f88ce46c308d0d4c7468efb274cbbc06
姚佩彤 <a151815058@gmail.com>
2019-11-14 21:14:35 +0800

    Added session tracker for dynmaic source links

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,11 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   include DeviseWhitelist
+
+  before_action :set_source
+
+  def set_source
+  	session[:source] = params[:q] if params[:q] #如果參數q存在，則把參數q傳到session的source裡
+  end
 end
  
