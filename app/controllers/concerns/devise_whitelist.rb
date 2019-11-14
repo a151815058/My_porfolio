bec2869a2a62ca863f0cc54fb680fad7bc9af014
姚佩彤 <a151815058@gmail.com>
2019-11-14 19:46:54 +0800

    Added controller concerns for devise params

---
+++ app/controllers/concerns/devise_whitelist.rb
@@ -0,0 +1,13 @@
+module DeviseWhitelist
+	extend ActiveSupport::Concern 
+
+	##在included devise_whitelist時，就會自動執行before_action.....
+	included do 
+		before_action :configure_permitted_parameters , if: :devise_controller?
+	end 
+
+	def configure_permitted_parameters
+  		devise_parameter_sanitizer.permit(:sign_up, keys: [:name])	
+  		devise_parameter_sanitizer.permit(:account_update, keys: [:name])	
+  	end
+end 
