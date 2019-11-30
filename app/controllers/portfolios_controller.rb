370189c6e63051775536c0d7def67a8659f7e3f3
姚佩彤 <a151815058@gmail.com>
2019-11-30 19:02:59 +0800

    Updated portfolio to include attr for position

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -4,7 +4,7 @@ class PortfoliosController < ApplicationController
   layout "portfolio"
 
 	def index
-		@portfolio_items = Portfolio.all
+		@portfolio_items = Portfolio.by_position
 
     @page_title ="My porttfolio website" 
 	end
